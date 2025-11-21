c378a4a5eed6a0a48b3896772b2d5582b27bc498
Tina <murp443@gmail.com>
2025-11-21 10:44:07 -0800

    line fit plot over time

--- altitude_helper.py
+++ altitude_helper.py
@@ -176,3 +176,82 @@ def plot_lat_lon(yknf_rgb_asi_ds, fsmi_rgb_asi_ds, time_index, site_name_yknf, s
     plt.show()
 
     return rgb_yknf_adjusted, rgb_fsmi_adjusted
+
+
+def mod_plot_lat_lon(yknf_rgb_asi_ds, fsmi_rgb_asi_ds, time_index, site_name_yknf, site_name_fsmi, yknf_lat, yknf_lon, fsmi_lat, fsmi_lon, h_target):
+    ''' 
+    Plots the projected latitude and longitude of the YKNF and FSMI, and overlays them. 
+    Produces 3 plots total: YKNF, FSMI, overlaid, and saves each of the images. 
+
+    Parameters:
+        yknf_rgb_asi_ds: xarray of the yknf skymap <xarray>
+        fsmi_rgb_asi_ds: xarray of the fsmi skymap <xarray>
+        time_index: specific frame we are looking at from the asi ds <int>
+        site_name_yknf: string to label the plots
+        site_name_fsmi: string to label the plots
+        yknf_lat: projected latitude array of one yknf frame <2d arr>
+        yknf_lon: projected longitude array of one yknf frame <2d arr>
+        fsmi_lat: projected latitude array of one fsmi frame <2d arr>
+        fsmi_lon: projected longitude array of one fsmi frame <2d arr>
+        h_target: height that yknf & fsmi frames were projected to <int>
+        lat_lon_plots: folder to save the 3 images to 
+
+    Outputs:
+        3 plots of the YKNF, FSMI, overlaid latitude and longitude
+    '''    
+    
+    R_yknf = yknf_rgb_asi_ds.image.sel(channel="R").isel(times=time_index).values
+    G_yknf = yknf_rgb_asi_ds.image.sel(channel="G").isel(times=time_index).values
+    B_yknf = yknf_rgb_asi_ds.image.sel(channel="B").isel(times=time_index).values
+    
+    R_fsmi = fsmi_rgb_asi_ds.image.sel(channel="R").isel(times=time_index).values
+    G_fsmi = fsmi_rgb_asi_ds.image.sel(channel="G").isel(times=time_index).values
+    B_fsmi = fsmi_rgb_asi_ds.image.sel(channel="B").isel(times=time_index).values
+    
+    
+    # Extract time and format it
+    raw_time = yknf_rgb_asi_ds.times.values[time_index]
+    time_obj = pd.to_datetime(raw_time.decode("utf-8").replace(" UTC", ""))
+    time_str = time_obj.strftime("%b. %d, %Y %H:%M:%S UT")
+        
+    # contrast adjustment: alpha=contrast, beta=brightness
+    alpha = 5
+    beta = 5
+    rgb_yknf = np.stack([R_yknf, G_yknf, B_yknf], axis=-1)  # shape: (x, y, 3)
+    rgb_fsmi = np.stack([R_fsmi, G_fsmi, B_fsmi], axis=-1)  # shape: (x, y, 3)
+    
+    rgb_yknf_adjusted = cv2.convertScaleAbs(rgb_yknf, alpha=alpha, beta=beta)
+    rgb_fsmi_adjusted = cv2.convertScaleAbs(rgb_fsmi, alpha=alpha, beta=beta)
+    
+    # # yknf projected
+    # fig1, ax1 = plt.subplots(figsize=(8,8))
+    # scat1 = ax1.scatter(yknf_lon.flatten(),yknf_lat.flatten(),c=rgb_yknf_adjusted.reshape(-1, 3)/256,s=1)
+    # #plt.xlim(ax_skymap.get_xlim())
+    # #plt.ylim(ax_skymap.get_ylim())
+    # ax1.set_ylabel("Latitude (deg)")
+    # ax1.set_xlabel("Longitude (deg)")
+    # ax1.set_title(f"{h_target/1000}km Projection: {site_name_yknf} – {time_str}", pad=30);
+    # plt.show()
+    
+    # # fsmi projected
+    # fig2, ax2 = plt.subplots(figsize=(8,8))
+    # scat2 = ax2.scatter(fsmi_lon.flatten(),fsmi_lat.flatten(),c=rgb_fsmi_adjusted.reshape(-1, 3)/256,s=1)
+    # #plt.xlim(ax_skymap.get_xlim())
+    # #plt.ylim(ax_skymap.get_ylim())
+    # ax2.set_ylabel("Latitude (deg)")
+    # ax2.set_xlabel("Longitude (deg)")
+    # ax2.set_title(f"{h_target/1000}km Projection: {site_name_fsmi} – {time_str}", pad=30);
+    # plt.show()
+    
+    # # 110km  overlaid --> 
+    # plt.figure(figsize=(8,8))
+    # plt.scatter(yknf_lon.flatten(),yknf_lat.flatten(),c=rgb_yknf_adjusted.reshape(-1, 3)/256,s=1, alpha=0.5)
+    # plt.scatter(fsmi_lon.flatten(),fsmi_lat.flatten(),c=rgb_fsmi_adjusted.reshape(-1, 3)/256,s=1, alpha=0.02)
+    # plt.xlabel("Longitude (deg)")
+    # plt.ylabel("Latitude (deg)")
+    # plt.title(f"Overlaid {h_target/1000}km Projection - {time_str}", pad=30)
+    # #plt.xlim(ax_skymap.get_xlim())
+    # #plt.ylim(ax_skymap.get_ylim())
+    # plt.show()
+
+    return rgb_yknf_adjusted, rgb_fsmi_adjusted
